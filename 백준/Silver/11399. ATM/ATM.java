e5721bbd7ff12c084caaa60fd7f3ce3002a6fbba
hhyukk <130235875+hhyukk@users.noreply.github.com>
2025-08-11 10:03:13 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,37 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
         int[] p = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
-        }
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) p[i] = Integer.parseInt(st.nextToken());
+
+        // 삽입 정렬: 오름차순
         for (int i = 1; i < N; i++) {
             int insertValue = p[i];
-            int insertIndex = i;
-            for (int j = i - 1; j >= 0; j--) {
-                if (p[i] > p[j]) {
-                    insertIndex = j + 1;
-                    break;
-                }
-                if (j == 0) {
-                    insertIndex = 0;
-                }
+            int j = i - 1;
+
+            // 왼쪽으로 가며 자신보다 큰 값들을 한 칸씩 뒤로 민다
+            while (j >= 0 && p[j] > insertValue) {
+                p[j + 1] = p[j];
+                j--;
             }
-            for (int j = i; j > insertIndex; j--) {
-                p[j] = p[j - 1];
-            }
-            p[insertIndex] = insertValue;
+            p[j + 1] = insertValue; // 알맞은 위치에 삽입
         }
-        int[] sum = new int[N];
-        sum[0] = p[0];
-        int sumValue = sum[0];
-        for (int i = 1; i < N; i++) {
-            sum[i] = sum[i - 1] + p[i];
-            sumValue += sum[i];
+
+        int prefix = 0, answer = 0;
+        for (int x : p) {
+            prefix += x;
+            answer += prefix;
         }
-        System.out.println(sumValue);
+        System.out.println(answer);
     }
 }
