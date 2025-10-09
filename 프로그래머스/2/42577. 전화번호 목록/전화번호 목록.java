e9f535760d03d4077c862f78f6a93ffb04d3a8b9
hhyukk <130235875+hhyukk@users.noreply.github.com>
2025-10-09 17:24:27 +0900

    [level 2] Title: 전화번호 목록, Time: 277.77 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,22 @@
 import java.util.*;
 
 class Solution {
+
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].indexOf(phone_book[i]) == 0) return false;
+        Set<String> set = new HashSet<>();
+
+        // 모든 전화번호를 HashSet에 저장
+        Arrays.stream(phone_book).forEach(set::add);
+
+        // 각 번호의 접두어(1 ~ length - 1)를 검사하여
+        // 다른 번호가 접두어로 존재하는 경우 false 반환
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                if (set.contains(phone.substring(0, i))) return false;
+            }
         }
+
+        // 접두어가 존재하지 않으면 true 반환
         return true;
     }
 }
