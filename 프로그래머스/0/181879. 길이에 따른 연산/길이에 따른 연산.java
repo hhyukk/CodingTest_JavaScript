fe81485a6391aa1b614e8af3be4a733b2ca2296d
hhyukk <130235875+hhyukk@users.noreply.github.com>
2025-11-17 17:48:16 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.72 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int result=1;
+        if(num_list.length >= 11){
+            for(int i=0; i<num_list.length; i++){
+                result+=num_list[i];
+            }
+            result-=1;
+        }else{
+            for(int i=0; i<num_list.length; i++){
+                result*=num_list[i];
+            }
+        }
+        return result;
+    }
+}
